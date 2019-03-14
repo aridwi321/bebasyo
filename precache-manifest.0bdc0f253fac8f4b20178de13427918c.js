741a3595259cde68dcb4da8034327e61c7caa028
unknown <dio.tridia@gmail.com>
2019-03-14 12:37:19 +0700

    Updates

---
+++ precache-manifest.0bdc0f253fac8f4b20178de13427918c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44d6d7a8eaf0382f64dd",
+    "url": "/bebasyo/static/js/runtime~main.44d6d7a8.js"
+  },
+  {
+    "revision": "8d0843bd403a0d7d0887",
+    "url": "/bebasyo/static/js/main.8d0843bd.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/bebasyo/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "8d0843bd403a0d7d0887",
+    "url": "/bebasyo/static/css/main.c53d617e.chunk.css"
+  },
+  {
+    "revision": "185089f351ada1f961056eaac3eecc57",
+    "url": "/bebasyo/index.html"
+  }
+];
